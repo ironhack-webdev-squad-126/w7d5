7c5c7cfac5bd9a6a4299d1bc42644b2153c914b4
mjarraya <mjarraya@student.42.fr>
2019-05-03 14:50:22 +0200

    first input

---
+++ pm-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AddProject from "./components/Projects/Add";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <AddProject />
+    </div>
+  );
+}
+
+export default App;
---
+++ pm-app/src/components/Projects/Add.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+class AddProject extends React.Component {
+  state = {
+    title: "",
+    description: ""
+  };
+
+  handleChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form>
+          <div>
+            <label>title:</label>
+            <input
+              name="title"
+              type="text"
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+          </div>
+          <input type="submit" value="Create a project" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddProject;
