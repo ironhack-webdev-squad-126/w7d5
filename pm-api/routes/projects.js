1e8c219cb490030e402b6157bd5c868212caaaba
mjarraya <mjarraya@student.42.fr>
2019-05-03 11:21:15 +0200

    project routes

--- pm-api/routes/projects.js
+++ pm-api/routes/projects.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Project = require("../models/Project");
+const Task = require("../models/Task");
 
 router.post("/projects", (req, res) => {
   Project.create({
@@ -10,6 +11,48 @@ router.post("/projects", (req, res) => {
   })
     .then(response => {
       res.json(response);
+    })
+    .catch(error => {
+      res.json(error);
+    });
+});
+
+router.get("/projects", (req, res) => {
+  Project.find({})
+    .populate("tasks")
+    .then(projects => {
+      res.json(projects);
+    })
+    .catch(error => {
+      res.json(error);
+    });
+});
+
+router.get("/projects/:id", (req, res) => {
+  Project.findById(req.params.id)
+    .populate("tasks")
+    .then(project => {
+      res.json(project);
+    })
+    .catch(error => {
+      res.json(error);
+    });
+});
+
+router.put("/projects/:id", (req, res) => {
+  Project.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.status(200).json({ message: "ok" });
+    })
+    .catch(error => {
+      res.json(error);
+    });
+});
+
+router.delete("/projects/:id", (req, res) => {
+  Project.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(200).json({ message: "ok" });
     })
     .catch(error => {
       res.json(error);
