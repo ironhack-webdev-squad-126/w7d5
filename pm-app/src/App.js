e650e37eb1c069867532b8c58822cfa4454db025
mjarraya <mjarraya@student.42.fr>
2019-05-03 15:58:01 +0200

    wip

--- pm-app/src/App.js
+++ pm-app/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import ProjectDetails from "./components/Projects/Details";
 import ProjectsList from "./components/Projects/List";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <ProjectsList />
+      <Switch>
+        <Route path="/projects/:id" component={ProjectDetails} />
+        <Route path="/projects" component={ProjectsList} />
+      </Switch>
     </div>
   );
 }
